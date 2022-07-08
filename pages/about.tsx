1bdc362422419451db7e6203836d7184b5176304
Heiki Riesenkampf <me@heikir.com>
2022-07-08 10:48:01 +0300

    fix(types): fix linting issues

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,15 @@
-import Link from 'next/link'
-import { Container, Main, Title, Description } from '../components/sharedstyles'
+import Link from "next/link";
+import { Container, Main, Title } from "../components/sharedstyles";
 
 export default function About() {
   return (
     <Container>
       <Main>
         <Title>About Page</Title>
-        <Description>
-          <Link href="/">
-            <a>&larr; Go Back</a>
-          </Link>
-        </Description>
+        <Link href="/">
+          <a>&larr; Go Back</a>
+        </Link>
       </Main>
     </Container>
-  )
+  );
 }
